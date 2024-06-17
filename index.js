e6e624a6d3f3e1ed75f357d8ac77cc5ed47df0b1
N01Knowz <ronronarnibal@gmail.com>
2024-06-18 02:38:56 +0800

    Allowed Multiple Origins

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors());
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://mernstackpractice-client.vercel.app",
-// ];
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true); // Allow requests with no Origin header
+// app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mernstackpractice-client.vercel.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Allow requests with no Origin header
 
-//       if (allowedOrigins.includes(origin)) {
-//         return callback(null, origin);
-//       } else {
-//         return callback(new Error("Origin not allowed by CORS"));
-//       }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "csrf-token"], // Include csrf-token in allowed headers
-//     credentials: true, // Allow credentials (cookies) to be sent
-//   })
-// );
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, origin);
+      } else {
+        return callback(new Error("Origin not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "csrf-token"], // Include csrf-token in allowed headers
+    credentials: true, // Allow credentials (cookies) to be sent
+  })
+);
 
 const csrfProtection = csurf({ cookie: true });
 app.use(csrfProtection);
